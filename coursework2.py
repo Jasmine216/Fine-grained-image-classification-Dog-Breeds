834ea3a3bb1d578cf76928f075bf918502e47214
Jasmine216 <56590213+Jasmine216@users.noreply.github.com>
2020-03-03 21:23:33 +0000

    Add files via upload
    change binary to catogorical

--- coursework2.py
+++ coursework2.py
@@ -118,8 +118,8 @@ for i in os.listdir(original_dataset_dir):
 from keras.preprocessing.image import ImageDataGenerator
 train_datagen=ImageDataGenerator(rescale=1./255)
 test_datagen=ImageDataGenerator(rescale=1./255)
-train_generator=train_datagen.flow_from_directory(train_dir,target_size=(150,150),batch_size=20,class_mode="binary")
-validation_generator=test_datagen.flow_from_directory(validation_dir,target_size=(150,150),batch_size=20,class_mode="binary")
+train_generator=train_datagen.flow_from_directory(train_dir,target_size=(150,150),batch_size=20,class_mode="categorical")
+validation_generator=test_datagen.flow_from_directory(validation_dir,target_size=(150,150),batch_size=20,class_mode="categorical")
 
 #%%
 from keras import layers
@@ -139,10 +139,10 @@ model.add(layers.Conv2D(128,(3,3),activation="relu")) #(15,15,128)
 model.add(layers.MaxPool2D(2,2)) #(7,7,128)
 model.add(layers.Flatten()) #7*7*128=6272
 model.add(layers.Dense(512,activation="relu"))
-model.add(layers.Dense(1,activation="sigmoid"))
+model.add(layers.Dense(120,activation="sigmoid"))
 model.summary()
 #%%
 #fit model
 from keras import optimizers
-model.compile(loss="binary_crossentropy",optimizer=optimizers.RMSprop(lr=1e-4),metrics=["acc"])
+model.compile(loss="categorical_crossentropy",optimizer=optimizers.RMSprop(lr=1e-4),metrics=["acc"])
 history=model.fit_generator(train_generator,steps_per_epoch=100,epochs=30,validation_data=validation_generator,validation_steps=50)
